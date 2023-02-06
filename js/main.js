81be150e4f47bb58fc7e3f6afdc10c96ec957412
devMarciano <luciano.m1996@gmail.com>
2023-02-06 19:18:53 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const robotron = document.querySelector('#robotron')
+
+robotron.addEventListener('click', dizOi)
+
+function dizOi() {
+    console.log('Olá humano')
+    console.log('Eu sou o robotron, lider dos DevRobots')
+}
+
+dizOi()
